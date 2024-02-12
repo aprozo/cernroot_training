df84d6cfe6ca04b5fd26d38bef98870542748f2c
Alexandr Prozorov <33087030+aprozo@users.noreply.github.com>
2024-02-12 22:38:39 +0100

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+source /opt/root/bin/thisroot.sh
 mkdir ~/.local/share/jupyter/kernels
-cp -r /opt/root/etc/notebook/kernels/root ~/.local/share/jupyter/kernels
+cp -r $ROOTSYS/etc/notebook/kernels/root ~/.local/share/jupyter/kernels
