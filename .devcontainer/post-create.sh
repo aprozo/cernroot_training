982ae627aedb1ab6c48f4aa209996d1bd9ba2f80
aprozo <alexandr.a.prozorov@gmail.com>
2024-02-15 09:56:27 +0100

    new user + start jupyter

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source /opt/root/bin/thisroot.sh
-mkdir ~/.local/share/jupyter/kernels
+mkdir -p ~/.local/share/jupyter/kernels
 cp -r $ROOTSYS/etc/notebook/kernels/root ~/.local/share/jupyter/kernels
